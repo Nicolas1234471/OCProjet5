9bd1f7b26a7eb444a13a2eb358170cd9a128bde2
Nicolas Dupouy <nicolas.dupouy17@orange.fr>
2025-01-23 11:44:36 +0100

    Changements du html et mise en place du carousel sans l'update des bulletpoints et sans la boucle infinie

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,67 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowleft = document.querySelector('.arrow_left')
+const arrowright = document.querySelector('.arrow_right')
+const bulletpoints = document.querySelector('.dots')
+const imagebanner = document.querySelector('.banner-img')
+const textbanner = document.querySelector('#banner p')
+const bulletpointslist = document.querySelectorAll('.dot')
+let indexslide = 0
+
+function initdot() {
+	slides.forEach((_, index) => {
+		const dot = document.createElement('div')
+		dot.classList.add('dot')
+		if (index === 0) {
+			dot.classList.add('dot_selected')
+		}
+		bulletpoints.appendChild(dot)
+	});
+}
+initdot();
+
+function updateDot() {
+	bulletpointslist.forEach((dot, index) => {
+		if (index === indexslide) {
+			dot.classList.add('dot_selected')
+		}
+		else {
+			dot.classList.remove('dot_selected')
+		}
+	})
+}
+
+arrowright.addEventListener("click", () => {
+	alert('Clic flèche de droite')
+	indexslide++
+	updateBanner()
+})
+
+arrowleft.addEventListener("click", () => {
+	alert('Clic flèche de gauche')
+	indexslide--
+	updateBanner()
+})
+
+function updateBanner() {
+	imagebanner.src = `./assets/images/slideshow/${slides[indexslide].image}`
+	textbanner.innerHTML = slides[indexslide].tagLine
+	updateDot()
+}
+
+
+/* function changebanner() {
+	slides.forEach((_, index) => {
+		if (index === 0) 
+			imagebanner.src = "./assets/images/slideshow/slide1.jpg"
+		else if (index === 1)
+			imagebanner.src = "./assets/images/slideshow/slide2.jpg"
+		else if (index === 2)
+			imagebanner.src = "./assets/images/slideshow/slide3.jpg"
+		else if (index === 3)
+			imagebanner.src = "./assets/images/slideshow/slide4.png"
+	} );
+}
+changebanner(); */
